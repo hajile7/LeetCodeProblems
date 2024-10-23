e763a8107b4532a07782f270d151d5224a2a0dda
Eli <elireid.28@gmail.com>
2024-10-23 12:26:01 -0400

    Added car fleet

--- LeetCode Problems/Program.cs
+++ LeetCode Problems/Program.cs
@@ -375,4 +375,31 @@ public class Solution
         }
         return res;
     }
+
+    static int CarFleet(int[] position, int[] speed, int target)
+    {
+        int n = position.Length;
+        double[][] pairs = new double[n][];
+        for (int i = 0; i < n; i++)
+        {
+            pairs[i] = new double[] { position[i], speed[i] };
+        }
+        Array.Sort(pairs, (a, b) => b[0].CompareTo(a[0]));
+
+        int fleetCount = 0;
+        double[] timeToReach = new double[n];
+        for (int i = 0; i < n; i++)
+        {
+            timeToReach[i] = (target - pairs[i][0]) / pairs[i][1];
+            if (i >= 1 && timeToReach[i] <= timeToReach[i - 1])
+            {
+                timeToReach[i] = timeToReach[i - 1];
+            }
+            else
+            {
+                fleetCount++;
+            }
+        }
+        return fleetCount;
+    }
 }
