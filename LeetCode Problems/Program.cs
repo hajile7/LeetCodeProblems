b7172f5a48aff8f7c5ec9c8ed40826b46736fb66
Eli <elireid.28@gmail.com>
2024-11-10 16:52:58 -0500

    Added trapping rainwater

--- LeetCode Problems/Program.cs
+++ LeetCode Problems/Program.cs
@@ -595,6 +595,34 @@ static int MaxArea(int[] heights)
     }
     return res;
 }
+// Trapping Rainwater
+static int Trap(int[] height)
+{
+    if (height == null || height.Length == 0)
+    {
+        return 0;
+    }
+
+    int l = 0, r = height.Length - 1;
+    int leftMax = height[l], rightMax = height[r];
+    int res = 0;
+    while (l < r)
+    {
+        if (leftMax < rightMax)
+        {
+            l++;
+            leftMax = Math.Max(leftMax, height[l]);
+            res += leftMax - height[l];
+        }
+        else
+        {
+            r--;
+            rightMax = Math.Max(rightMax, height[r]);
+            res += rightMax - height[r];
+        }
+    }
+    return res;
+}
 
 public class Solution
 {
@@ -630,3 +658,5 @@ public class Solution
     }
 
 }
+
+
