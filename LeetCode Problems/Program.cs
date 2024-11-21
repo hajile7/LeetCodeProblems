67dad2012d03502c5e2c9f78fffb70cece85f8f2
Eli <elireid.28@gmail.com>
2024-11-21 15:17:27 -0500

    Added minimum window substring problem

--- LeetCode Problems/Program.cs
+++ LeetCode Problems/Program.cs
@@ -750,6 +750,69 @@ static bool CheckInclusion(string s1, string s2)
     return matches == 26;
 }
 
+static string MinWindow(string s, string t)
+{
+    if (t == "") return "";
+
+    Dictionary<char, int> countT = new Dictionary<char, int>();
+    Dictionary<char, int> window = new Dictionary<char, int>();
+
+    foreach (char c in t)
+    {
+        if (countT.ContainsKey(c))
+        {
+            countT[c]++;
+        }
+        else
+        {
+            countT[c] = 1;
+        }
+    }
+
+    int have = 0, need = countT.Count;
+    int[] res = { -1, -1 };
+    int resLen = int.MaxValue;
+    int l = 0;
+
+    for (int r = 0; r < s.Length; r++)
+    {
+        char c = s[r];
+        if (window.ContainsKey(c))
+        {
+            window[c]++;
+        }
+        else
+        {
+            window[c] = 1;
+        }
+
+        if (countT.ContainsKey(c) && window[c] == countT[c])
+        {
+            have++;
+        }
+
+        while (have == need)
+        {
+            if ((r - l + 1) < resLen)
+            {
+                resLen = r - l + 1;
+                res[0] = l;
+                res[1] = r;
+            }
+
+            char leftChar = s[l];
+            window[leftChar]--;
+            if (countT.ContainsKey(leftChar) && window[leftChar] < countT[leftChar])
+            {
+                have--;
+            }
+            l++;
+        }
+    }
+
+    return resLen == int.MaxValue ? "" : s.Substring(res[0], resLen);
+}
+
 public class Solution
 {
     //Create an entry point to our recursive function
