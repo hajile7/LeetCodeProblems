7a882bea4dfd375357d3451573e83c7d50a52d1e
Eli <elireid.28@gmail.com>
2024-11-22 12:33:36 -0500

    Added maximum sliding window problem

--- LeetCode Problems/Program.cs
+++ LeetCode Problems/Program.cs
@@ -813,6 +813,37 @@ static string MinWindow(string s, string t)
     return resLen == int.MaxValue ? "" : s.Substring(res[0], resLen);
 }
 
+static int[] MaxSlidingWindow(int[] nums, int k)
+{
+    int n = nums.Length;
+    int[] output = new int[n - k + 1];
+    var q = new LinkedList<int>();
+    int l = 0, r = 0;
+
+    while (r < n)
+    {
+        while (q.Count > 0 && nums[q.Last.Value] < nums[r])
+        {
+            q.RemoveLast();
+        }
+        q.AddLast(r);
+
+        if (l > q.First.Value)
+        {
+            q.RemoveFirst();
+        }
+
+        if ((r + 1) >= k)
+        {
+            output[l] = nums[q.First.Value];
+            l++;
+        }
+        r++;
+    }
+
+    return output;
+}
+
 public class Solution
 {
     //Create an entry point to our recursive function
