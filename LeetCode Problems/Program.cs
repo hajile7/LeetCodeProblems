1f5f25ba427671747707311edb9bc1072cacacd6
Eli <elireid.28@gmail.com>
2024-11-25 18:33:49 -0500

    Added binary search problem

--- LeetCode Problems/Program.cs
+++ LeetCode Problems/Program.cs
@@ -844,6 +844,29 @@ static int[] MaxSlidingWindow(int[] nums, int k)
     return output;
 }
 
+static int IterativeBinarySearch(int[] nums, int target)
+{
+    int l = 0, r = nums.Length - 1;
+
+    while (l <= r)
+    {
+        int m = l + ((r - l) / 2);
+        if (nums[m] > target)
+        {
+            r = m - 1;
+        }
+        else if (nums[m] < target)
+        {
+            l = m + 1;
+        }
+        else
+        {
+            return m;
+        }
+    }
+    return -1;
+}
+
 public class Solution
 {
     //Create an entry point to our recursive function
